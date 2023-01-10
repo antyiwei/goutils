eefaf5c449cf542faeb7f83d8d13c16f30fe4b9a
yiwei <1032520598@qq.com>
2023-01-10 14:46:14 +0800

    fix num utils

--- numutils/num.go
+++ numutils/num.go
@@ -1,6 +1,9 @@
 package numutils
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 func SubPhone(phone string) string {
 
@@ -20,4 +23,40 @@ func SubPhone(phone string) string {
 		return strings.Replace(phone, "0086", "", 1)
 	}
 	return phone
+}
+
+const (
+	NumberReg = `^0|9\d{2,3}-?\d{7,8}}|400-?[016789]-?\d{6}$`                 // 固话
+	MobileRex = `^1(3\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\d|9[0-35-9])\d{8}$` // 手机号
+)
+
+func ValidateNumber(number string) bool {
+	result := validateNum(number)
+	if result {
+		return true
+	}
+
+	result = validateMobile(number)
+	if result {
+		return true
+	}
+	return false
+}
+
+func validateNum(number string) bool {
+	reg := regexp.MustCompile(NumberReg)
+	result := reg.MatchString(number)
+	if result {
+		return true
+	}
+	return false
+}
+
+func validateMobile(number string) bool {
+	reg := regexp.MustCompile(MobileRex)
+	result := reg.MatchString(number)
+	if result {
+		return true
+	}
+	return false
 }
