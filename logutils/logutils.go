ffb427781b60fe5e95b13d6fe53b949545713e7e
易伟 <1032520598@qq.com>
2023-04-22 20:26:34 +0800

    add log

---
+++ logutils/logutils.go
@@ -0,0 +1,19 @@
+package logutils
+
+import (
+	"encoding/json"
+	"log"
+)
+
+// 打印更漂亮的对象
+func PrintObj(obj interface{}) {
+
+	log.Println(FormatJSON(obj))
+
+}
+
+func FormatJSON(obj interface{}) string {
+	jsIndent, _ := json.MarshalIndent(&obj, "", "\t")
+
+	return string(jsIndent)
+}
---
+++ mongoutils/mongo.go
@@ -0,0 +1,38 @@
+package mongoutils
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoSetting struct {
+	URI          string
+	DatabaseName string
+}
+
+
+var uri string, databaseName string
+func Get() MongoSetting {
+	return MongoSetting{
+		URI:          uri,
+		DatabaseName: databaseName,
+	}
+}
+
+// ConnectToDB starts a new database connection and returns a reference to it
+func ConnectToDB() (*mongo.Database, error) {
+	settings := Get()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	options := options.Client().ApplyURI(settings.URI)
+	options.SetMaxPoolSize(10)
+	client, err := mongo.Connect(ctx, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Database(settings.DatabaseName), nil
+}
