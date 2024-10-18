894e09c3357e9662206ccd67ed38c5eb82ce1e7f
易伟 <1032520598@qq.com>
2024-10-18 10:58:52 +0800

    add debugutils

---
+++ debugutils/debugutils.go
@@ -0,0 +1,49 @@
+package debugutils
+
+import (
+	"log"
+	"os"
+
+	"github.com/antyiwei/goutils/logutils"
+)
+
+var isDebug = false
+
+func init() {
+
+	if os.Getenv("DEBUG") == "1" {
+		isDebug = true
+	}
+}
+
+func SetDebug(flag bool) {
+	isDebug = flag
+}
+
+func IsDebugging() bool {
+
+	return isDebug
+}
+
+func Println(v ...interface{}) {
+	if isDebug {
+
+		log.Println(v...)
+	}
+}
+
+func PrintObj(v interface{}) {
+	if isDebug {
+
+		logutils.PrintObj(v)
+	}
+}
+
+func Printf(format string, v ...interface{}) {
+
+	if isDebug {
+		log.Printf(format, v...)
+
+	}
+
+}
